f73479966d26c4491240d4b2b4b569a75ef22497
minios <minios.flow@gmail.com>
2026-08-18 13:32:18 +0900

    :sparkles: feat: SwiftUI 프로젝트 생성.

---
+++ Weather-TCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather-TCA
+//
+//  Created by 민경준 on 8/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather-TCA/Weather_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_TCAApp.swift
+//  Weather-TCA
+//
+//  Created by 민경준 on 8/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Weather_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
